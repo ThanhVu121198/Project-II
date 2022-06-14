9ef17c81ce00671abd2dec00af9c7364ff00248a
Vu_Duc_Thanh <thanhvu121198@gmail.com>
2022-06-14 11:25:52 +0700

    Thanh Commit

--- ProjectII/app/Http/Controllers/Front/ShopController.php
+++ ProjectII/app/Http/Controllers/Front/ShopController.php
@@ -9,17 +9,19 @@ use App\Models\ProductComment;
 class ShopController extends Controller
 {
     public function show($id) {
-
         $product = Product::findOrFail($id);
 
         $avgRating = 0;
         $sumRating = array_sum(array_column($product->productComments->toArray(), 'rating'));
         $countRating = count($product->productComments);
+
         if ($countRating != 0) {
             $avgRating = $sumRating/$countRating;
         }
 
-        return view('front.shop.show', compact('product','avgRating'));
+        $relatedProducts = Product::where('product_category_id', $product->product_category_id)->where('tag', $product->tag)->limit(4)->get();
+
+        return view('front.shop.show', compact('product','avgRating','relatedProducts'));
     }
 
     public function postComment(Request $request) {
@@ -30,6 +32,10 @@ class ShopController extends Controller
 
 
     public function index() {
-        return view('front.shop.index');
+        
+        $products = Product::all();
+
+        return view('front.shop.index', compact('products'));
     }
+
 }
